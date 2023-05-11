4fe7e03db75a86ba4916960fa63eaaf21efa746c
BCarnaval <antoinedelagrave@hotmail.com>
2023-05-11 13:46:29 -0400

    style: change column_width from 120 to 80 and indent_width from 2 to 4 in .stylua.toml style: change textwidth from 80 and shiftwidth from 4 in options.lua feat: add catppuccin-macchiato colorscheme in options.lua feat: add lua_ls, pyright, and buffer_picker mappings in mappings.lua feat: add vim-asymptote, vimtex, and vim-better-whitespace plugins in user.lua refactor: disable stylua and enable lua_ls in mason.lua refactor: enable stylua and disable prettier in mason.lua refactor: enable python in mason.lua refactor: enable lua in treesitter.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,30 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function() require("lsp_signature").setup() end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppucin",
+    },
+    {
+        "hura/vim-asymptote",
+        name = "vim-asymptote",
+        lazy = false,
+    },
+    {
+        "lervag/vimtex",
+        ft = "tex",
+        lazy = false,
+    },
+    {
+        "ntpeters/vim-better-whitespace",
+        name = "vim-better-whitespace",
+        config = function() vim.g.better_whitespace_guicolor = "#FAC898" end,
+        lazy = false,
+    },
 }
